aa9e161f7e52a491e06711b9d26e512b0b2f44c8
ihipop <ihipop@gmail.com>
2018-11-21 13:41:54 +0800

    sprintf

--- build-phar.php
+++ build-phar.php
@@ -1,18 +1,18 @@
 <?php
+
 if (Phar::canWrite()) {
     $dir = realpath(__DIR__ . '/output/');
-    echo "Set target Dir $dir" . PHP_EOL;
+    printf("Set target Dir %s" . PHP_EOL,$dir);
     $pharFile = 'swoole-ide-helper.phar';
     $phar     = new Phar($pharFile, FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME, $pharFile);
     $phar->startBuffering();
     $phar->buildFromDirectory($dir, '/\.php$/');
     $phar->stopBuffering();
-    echo sprintf("\033[0;32m" . 'Success: Packed PHP files in %s to %s ' . "\033[0m", $dir, $pharFile) . PHP_EOL;
+    printf("\033[0;32m" . 'Success: Packed PHP files in %s to %s ' . "\033[0m".PHP_EOL, $dir, $pharFile);
 } else {
-    echo sprintf("\033[0;31m" . 'Error: `phar.readonly` is set to true,this should run with `phar.readonly` disabled.
-Use:
+    printf("\033[0;31m" . 'Error: `phar.readonly` is set to true,run me with  `phar.readonly` disabled
      php -d phar.readonly=0 %s
-to retry this command.' . "\033[0m", __FILE__) . PHP_EOL;
+to retry this command' . "\033[0m".PHP_EOL, __FILE__) ;
     exit(1);
 }
 exit(0);
