447bc776f3467031979675de9ff18cca4e666869
Demin Yin <deminy@deminy.net>
2021-04-09 08:39:25 -0700

    updates for Swoole 4.4.25

--- output/swoole/constants.php
+++ output/swoole/constants.php
@@ -1,10 +1,10 @@
 <?php
 
-define('SWOOLE_VERSION', '4.4.24');
-define('SWOOLE_VERSION_ID', 40424);
+define('SWOOLE_VERSION', '4.4.25');
+define('SWOOLE_VERSION_ID', 40425);
 define('SWOOLE_MAJOR_VERSION', 4);
 define('SWOOLE_MINOR_VERSION', 4);
-define('SWOOLE_RELEASE_VERSION', 24);
+define('SWOOLE_RELEASE_VERSION', 25);
 define('SWOOLE_EXTRA_VERSION', '');
 define('SWOOLE_DEBUG', '');
 define('SWOOLE_HAVE_COMPRESSION', '1');
