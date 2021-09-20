8dabf3d38b2efe9d220b69204dc362f149291b3e
Demin Yin <deminy@deminy.net>
2021-09-20 14:50:27 -0700

    documentation updates for method \Swoole\Runtime::enableCoroutine()

--- src/swoole/Swoole/Runtime.php
+++ src/swoole/Swoole/Runtime.php
@@ -7,6 +7,18 @@ namespace Swoole;
 class Runtime
 {
     /**
+     * To enable/disable runtime hooks in coroutines.
+     *
+     * For backward-compatible reason, there are four different ways to call this method:
+     *   #1. Swoole\Runtime::enableCoroutine();             // Enable runtime hooks represented by constant SWOOLE_HOOK_ALL.
+     *   #2. Swoole\Runtime::enableCoroutine($flags);       // Enable specified runtime hooks.
+     *   #3. Swoole\Runtime::enableCoroutine(true, $flags); // Enable specified runtime hooks.
+     *   #4. Swoole\Runtime::enableCoroutine(false);        // Disable runtime hooks.
+     * Following statements are of the same (when used to disable runtime hooks):
+     *   Swoole\Runtime::enableCoroutine(0);       // #2
+     *   Swoole\Runtime::enableCoroutine(true, 0); // #3
+     *   Swoole\Runtime::enableCoroutine(false);   // #4
+     *
      * @param int|bool $enable
      * @return bool true on success or false on failure
      */
