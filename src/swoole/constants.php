b277ed171a29f7bbce1c5feee5581553101e06bf
Demin Yin <deminy@deminy.net>
2022-01-11 09:42:26 -0800

    updates for Swoole 4.8.6
    Signed-off-by: Demin Yin <deminy@deminy.net>

--- src/swoole/constants.php
+++ src/swoole/constants.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-define('SWOOLE_VERSION', '4.8.5');
-define('SWOOLE_VERSION_ID', 40805);
+define('SWOOLE_VERSION', '4.8.6');
+define('SWOOLE_VERSION_ID', 40806);
 define('SWOOLE_MAJOR_VERSION', 4);
 define('SWOOLE_MINOR_VERSION', 8);
-define('SWOOLE_RELEASE_VERSION', 5);
+define('SWOOLE_RELEASE_VERSION', 6);
 define('SWOOLE_EXTRA_VERSION', '');
 define('SWOOLE_DEBUG', '');
 define('SWOOLE_HAVE_COMPRESSION', '1');
