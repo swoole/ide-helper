2087b68c321ec86dd5a3fb6c76e3954e5436e152
Demin Yin <deminy@deminy.net>
2025-01-03 22:29:06 -0800

    document class \Swoole\Thread\Queue
    Signed-off-by: Demin Yin <deminy@deminy.net>

--- src/swoole/Swoole/Thread/Queue.php
+++ src/swoole/Swoole/Thread/Queue.php
@@ -14,26 +14,54 @@ namespace Swoole\Thread;
  */
 final class Queue implements \Countable
 {
+    /**
+     * Unblock only one of the waiting threads.
+     *
+     * This constant is used by method Queue::push() only.
+     */
     public const NOTIFY_ONE = 1;
 
+    /**
+     * Unblocks all threads currently waiting for this queue.
+     *
+     * This constant is used by method Queue::push() only.
+     */
     public const NOTIFY_ALL = 2;
 
-    public function __construct()
-    {
-    }
-
+    /**
+     * Push a value into the queue.
+     *
+     * @param mixed $value The value to push into the queue.
+     * @param int $notify_which How to unblock threads that are waiting on the queue. Either NOTIFY_ONE, NOTIFY_ALL, or 0 (not to notify anyone).
+     */
     public function push(mixed $value, int $notify_which = 0): void
     {
     }
 
+    /**
+     * Pop a value from the queue.
+     *
+     * @param float $wait The maximum time, in seconds, to wait for a value to become available in the queue.
+     *                    A value of 0 means to wait indefinitely.
+     * @return mixed The value removed from the queue, or null if the wait time expires and no value is available.
+     */
     public function pop(float $wait = 0): mixed
     {
     }
 
+    /**
+     * Clean the queue by removing all elements from it.
+     */
     public function clean(): void
     {
     }
 
+    /**
+     * Count the number of elements in the queue.
+     *
+     * {@inheritDoc}
+     * @return int The number of elements in the queue.
+     */
     public function count(): int
     {
     }
